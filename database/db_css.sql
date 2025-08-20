a697ed2558dbdae2edab868c342ec96203f7f7af
Jaymilyn Aramil <aramiljaymie1214@gmail.com>
2025-08-20 19:57:00 +0800

    20/08/2025 Jaymilyn Aramil DD/MM/YYYY

---
+++ database/db_css.sql
@@ -0,0 +1,130 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 20, 2025 at 01:22 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_css`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `credentials`
+--
+
+CREATE TABLE `credentials` (
+  `user_id` int(11) NOT NULL,
+  `first_name` text NOT NULL,
+  `middle_name` text NOT NULL,
+  `last_name` text NOT NULL,
+  `campus` text NOT NULL,
+  `dp` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_choices`
+--
+
+CREATE TABLE `tbl_choices` (
+  `choices_id` int(11) NOT NULL,
+  `question_id` int(50) NOT NULL,
+  `choice_text` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_questionaire`
+--
+
+CREATE TABLE `tbl_questionaire` (
+  `question_id` int(11) NOT NULL,
+  `question_survey` varchar(200) NOT NULL,
+  `section` varchar(200) NOT NULL,
+  `question` varchar(200) NOT NULL,
+  `status` int(50) NOT NULL,
+  `question_type` varchar(100) NOT NULL,
+  `required` int(50) NOT NULL,
+  `header` int(50) NOT NULL,
+  `transaction_type` int(50) NOT NULL,
+  `question_rendering` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `credentials`
+--
+ALTER TABLE `credentials`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `tbl_choices`
+--
+ALTER TABLE `tbl_choices`
+  ADD PRIMARY KEY (`choices_id`),
+  ADD KEY `fk_question_id` (`question_id`);
+
+--
+-- Indexes for table `tbl_questionaire`
+--
+ALTER TABLE `tbl_questionaire`
+  ADD PRIMARY KEY (`question_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `credentials`
+--
+ALTER TABLE `credentials`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_choices`
+--
+ALTER TABLE `tbl_choices`
+  MODIFY `choices_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_questionaire`
+--
+ALTER TABLE `tbl_questionaire`
+  MODIFY `question_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tbl_choices`
+--
+ALTER TABLE `tbl_choices`
+  ADD CONSTRAINT `fk_question_id` FOREIGN KEY (`question_id`) REFERENCES `tbl_choices` (`choices_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
