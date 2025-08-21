9df555cdb7f12a7a0533122b03f84ecc1a9008c6
KuyaMaki <benitomarkjosh07@gmail.com>
2025-08-21 19:34:31 +0800

    21/08/2025 DD/MM/YYYY - Mark Josh Benito

--- pages/edit-survey/edit-survey.php
+++ pages/edit-survey/edit-survey.php
@@ -4,17 +4,20 @@
 
 
 
-  <table class="border px-4 py-2 border-[#1E1E1ECC] shadow-lg rounded-md overflow-hidden">
+  <table class="border px-4 py-2 border-[#1E1E1ECC] shadow-lg  overflow-hidden">
     <thead class="bg-[#064089] text-white font-normal">
     <tr>
-      <th class="border px-4 py-2 border-[#1E1E1ECC]">#</th>
-      <th class="border px-4 py-2 border-[#1E1E1ECC]">Questionnaire</th>
-      <th class="border px-4 py-2 border-[#1E1E1ECC]">Date Created</th>
-      <th class="border px-4 py-2 border-[#1E1E1ECC]">Date Approved</th>
-      <th class="border px-4 py-2 border-[#1E1E1ECC]">Change Log</th>
-      <th class="border px-4 py-2 border-[#1E1E1ECC]">Actions</th>
+      <th class="border px-4 py-3 border-[#1E1E1ECC]">#</th>
+      <th class="border px-4 py-3 border-[#1E1E1ECC]">Questionnaire</th>
+      <th class="border px-4 py-3 border-[#1E1E1ECC]">Date Created</th>
+      <th class="border px-4 py-3 border-[#1E1E1ECC]">Date Approved</th>
+      <th class="border px-4 py-3 border-[#1E1E1ECC]">Change Log</th>
+      <th class="border px-4 py-3 border-[#1E1E1ECC]">Actions</th>
     </tr>
   </thead>
 
   </table>
+  <div class="mt-4">
+    <button class="border px-4 py-1 border-[#000000cc] shadow-lg font-bold rounded-md"> + Add New Questionnaire</button>
+  </>
 </div>
