315e3aa48275bde19f194d4676173578ece523b2
Exodus102 <145414466+Exodus102@users.noreply.github.com>
2025-08-26 09:27:18 +0800

    26/08/2025 DD/MM/YYYY - Jenrick Aran

--- database/db_css.sql
+++ database/db_css.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 20, 2025 at 01:22 PM
+-- Generation Time: Aug 26, 2025 at 03:26 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -33,10 +33,18 @@ CREATE TABLE `credentials` (
   `middle_name` text NOT NULL,
   `last_name` text NOT NULL,
   `campus` text NOT NULL,
+  `type` varchar(100) NOT NULL,
   `dp` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `credentials`
+--
+
+INSERT INTO `credentials` (`user_id`, `first_name`, `middle_name`, `last_name`, `campus`, `type`, `dp`, `email`, `password`) VALUES
+(1, 'Jenrick', 'Panopio', 'Aran', 'Binangonan', 'Campus Director', '', 'aranjenrick@gmail.com', 'b9fbm4ya');
 
 -- --------------------------------------------------------
 
@@ -49,6 +57,14 @@ CREATE TABLE `tbl_choices` (
   `question_id` int(50) NOT NULL,
   `choice_text` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_choices`
+--
+
+INSERT INTO `tbl_choices` (`choices_id`, `question_id`, `choice_text`) VALUES
+(1, 1, 'Cardona'),
+(2, 1, 'Binangonan');
 
 -- --------------------------------------------------------
 
@@ -68,6 +84,33 @@ CREATE TABLE `tbl_questionaire` (
   `transaction_type` int(50) NOT NULL,
   `question_rendering` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_questionaire`
+--
+
+INSERT INTO `tbl_questionaire` (`question_id`, `question_survey`, `section`, `question`, `status`, `question_type`, `required`, `header`, `transaction_type`, `question_rendering`) VALUES
+(1, '2025 Questionaire_V1.0', 'Section 1', 'Campus', 1, 'Dropdown', 1, 1, 2, 'None');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `two_factor_codes`
+--
+
+CREATE TABLE `two_factor_codes` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `code` varchar(255) NOT NULL,
+  `expires_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `two_factor_codes`
+--
+
+INSERT INTO `two_factor_codes` (`id`, `user_id`, `code`, `expires_at`) VALUES
+(22, 1, '200122', '2025-08-26 03:21:06');
 
 --
 -- Indexes for dumped tables
@@ -93,6 +136,13 @@ ALTER TABLE `tbl_questionaire`
   ADD PRIMARY KEY (`question_id`);
 
 --
+-- Indexes for table `two_factor_codes`
+--
+ALTER TABLE `two_factor_codes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -100,19 +150,25 @@ ALTER TABLE `tbl_questionaire`
 -- AUTO_INCREMENT for table `credentials`
 --
 ALTER TABLE `credentials`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `tbl_choices`
 --
 ALTER TABLE `tbl_choices`
-  MODIFY `choices_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `choices_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `tbl_questionaire`
 --
 ALTER TABLE `tbl_questionaire`
-  MODIFY `question_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `question_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `two_factor_codes`
+--
+ALTER TABLE `two_factor_codes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- Constraints for dumped tables
@@ -122,7 +178,13 @@ ALTER TABLE `tbl_questionaire`
 -- Constraints for table `tbl_choices`
 --
 ALTER TABLE `tbl_choices`
-  ADD CONSTRAINT `fk_question_id` FOREIGN KEY (`question_id`) REFERENCES `tbl_choices` (`choices_id`);
+  ADD CONSTRAINT `fk_question_id` FOREIGN KEY (`question_id`) REFERENCES `tbl_questionaire` (`question_id`);
+
+--
+-- Constraints for table `two_factor_codes`
+--
+ALTER TABLE `two_factor_codes`
+  ADD CONSTRAINT `two_factor_codes_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `credentials` (`user_id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
