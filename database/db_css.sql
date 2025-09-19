8ae63219b8a90cbeca0875f6cfad22bfc6596f62
Exodus102 <145414466+Exodus102@users.noreply.github.com>
2025-09-19 17:25:57 +0800

    19/09/2025 DD/MM/YYYY - Jenrick Aran

--- database/db_css.sql
+++ database/db_css.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 14, 2025 at 02:54 PM
+-- Generation Time: Sep 19, 2025 at 11:25 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -49,8 +49,9 @@ CREATE TABLE `credentials` (
 
 INSERT INTO `credentials` (`user_id`, `first_name`, `middle_name`, `last_name`, `contact_number`, `campus`, `unit`, `type`, `dp`, `email`, `password`, `status`, `date_created`) VALUES
 (1, 'Jenrick', 'Panopio', 'Aran', '09158100920', 'Binangonan', 'University MIS', 'University MIS', '', 'aranjenrick@gmail.com', 'polskie123', 'Active', '0000-00-00'),
-(6, 'Jenrick', 'Dela Cruz', 'Aran', '09208256071', 'Binangonan', 'Registrar', 'University MIS', '', 'aaaaaa@gmail.com', 'bdsadasdsad', 'Active', '2025-09-12'),
-(7, 'Jenrick', 'Dela Cruz', 'Aran', '09208256071', 'Morong', 'Registrar', 'CSS Coordinator', '', 'ferf96989@gmail.com', 'polskie123', 'Active', '2025-09-12');
+(6, 'Jenrick', 'Dela Cruz', 'Aran', '09208256071', 'Binangonan', 'Campus Directors', 'Campus Director', '', 'aaaaaa@gmail.com', 'bdsadasdsad', 'Active', '2025-09-12'),
+(7, 'Jenrick', 'Dela Cruz', 'Aran', '09208256071', 'Binangonan', 'Registrar', 'CSS Coordinator', '', 'ferf96989@gmail.com', 'polskie123', 'Active', '2025-09-12'),
+(8, 'Ramirr', 'Oppus', 'Villamarin', '09158100920', 'Binangonan', 'College of Computer Studies', 'CSS Coordinator', '', 'dlhor65@gmail.com', 'polskie123', 'Active', '2025-09-18');
 
 -- --------------------------------------------------------
 
@@ -106,33 +107,6 @@ CREATE TABLE `tbl_choices` (
   `choice_text` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `tbl_choices`
---
-
-INSERT INTO `tbl_choices` (`choices_id`, `question_id`, `choice_text`) VALUES
-(2, 3, 'asdasdasdasdas'),
-(195, 67, '5'),
-(196, 67, '4'),
-(197, 67, '3'),
-(198, 67, '2'),
-(199, 67, '1'),
-(200, 68, '5'),
-(201, 68, '4'),
-(202, 68, '3'),
-(203, 68, '2'),
-(204, 68, '1'),
-(205, 69, '5'),
-(206, 69, '4'),
-(207, 69, '3'),
-(208, 69, '2'),
-(209, 69, '1'),
-(210, 70, '5'),
-(211, 70, '4'),
-(212, 70, '3'),
-(213, 70, '2'),
-(214, 70, '1');
-
 -- --------------------------------------------------------
 
 --
@@ -175,7 +149,8 @@ INSERT INTO `tbl_division` (`id`, `division_name`) VALUES
 (1, 'Office of The President'),
 (2, 'Academic Affairs'),
 (3, 'Administration and Finance Division'),
-(4, 'Research, Development, Extension, and Production Development');
+(4, 'Research, Development, Extension, and Production Development'),
+(6, 'Top Management');
 
 -- --------------------------------------------------------
 
@@ -196,20 +171,6 @@ CREATE TABLE `tbl_questionaire` (
   `question_rendering` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `tbl_questionaire`
---
-
-INSERT INTO `tbl_questionaire` (`question_id`, `question_survey`, `section`, `question`, `status`, `question_type`, `required`, `header`, `transaction_type`, `question_rendering`) VALUES
-(3, '2025 Questionaire_v1.3', 'Section 2', 'asdasdas', 0, 'Dropdown', 1, 0, 2, 'None'),
-(64, '2025 Questionaire_v1.2', 'Section 2', 'Name (Optional)', 1, 'Text', 0, 0, 2, 'None'),
-(65, '2025 Questionaire_v1.2', 'Section 2', 'Contact No. (Optional)', 1, 'Text', 0, 0, 2, 'None'),
-(66, '2025 Questionaire_v1.2', 'Section 2', 'Click on the corresponding to your answer using the given scale below', 1, 'Description', 1, 0, 2, 'None'),
-(67, '2025 Questionaire_v1.2', 'Section 2', 'a. Knowledge of the Job', 1, 'Multiple Choice', 1, 0, 0, 'QoS'),
-(68, '2025 Questionaire_v1.2', 'Section 2', 'b. Accuracy in providing information', 1, 'Multiple Choice', 1, 0, 0, 'QoS'),
-(69, '2025 Questionaire_v1.2', 'Section 2', 'c. Delivery of prompt and appropriate service', 1, 'Multiple Choice', 1, 0, 0, 'QoS'),
-(70, '2025 Questionaire_v1.2', 'Section 2', 'd. Professionalism and skillfulness of the service personnel', 1, 'Multiple Choice', 1, 0, 0, 'QoS');
-
 -- --------------------------------------------------------
 
 --
@@ -224,13 +185,17 @@ CREATE TABLE `tbl_questionaireform` (
   `timestamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `tbl_questionaireform`
+-- Table structure for table `tbl_report`
 --
 
-INSERT INTO `tbl_questionaireform` (`id`, `question_survey`, `change_log`, `date_approved`, `timestamp`) VALUES
-(1, '2025 Questionaire_v1.2', 'Updated survey questions and/or name.', NULL, '2025-09-09 17:26:34'),
-(2, '2025 Questionaire_v1.3', 'Initial survey creation.', NULL, '2025-09-08 15:34:56');
+CREATE TABLE `tbl_report` (
+  `id` int(11) NOT NULL,
+  `file_path` varchar(255) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -255,6 +220,18 @@ CREATE TABLE `tbl_responses` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbl_tally_report`
+--
+
+CREATE TABLE `tbl_tally_report` (
+  `id` int(11) NOT NULL,
+  `file_path` varchar(255) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbl_unit`
 --
 
@@ -271,7 +248,17 @@ CREATE TABLE `tbl_unit` (
 
 INSERT INTO `tbl_unit` (`id`, `campus_name`, `division_name`, `unit_name`) VALUES
 (1, 'Morong', 'Office of The President', 'Campus Management Information System'),
-(2, 'Morong', 'Office of The President', 'Campus Planning, Monitoring and Evaluation');
+(2, 'Morong', 'Office of The President', 'Campus Planning, Monitoring and Evaluation'),
+(3, 'Binangonan', 'Office of The President', 'Campus Planning, Monitoring and Evaluation'),
+(4, 'Binangonan', 'Office of The President', 'Campus Management Information System'),
+(5, 'Binangonan', 'Top Management', 'Campus Directors'),
+(6, 'Binangonan', 'Academic Affairs', 'College of Accountancy'),
+(7, 'Binangonan', 'Academic Affairs', 'College of Business'),
+(8, 'Binangonan', 'Academic Affairs', 'College of Computer Studies'),
+(9, 'Binangonan', 'Administration and Finance Division', 'Internal Audit Services'),
+(10, 'Binangonan', 'Research, Development, Extension, and Production Development', 'Campus Research'),
+(11, 'Binangonan', 'Academic Affairs', 'College of Social Work and Community Development'),
+(12, 'Binangonan', 'Academic Affairs', 'Graduate School');
 
 -- --------------------------------------------------------
 
@@ -291,7 +278,28 @@ CREATE TABLE `tbl_unit_mis` (
 
 INSERT INTO `tbl_unit_mis` (`id`, `division_name`, `unit_name`) VALUES
 (1, 'Office of The President', 'Campus Management Information System'),
-(2, 'Office of The President', 'Campus Planning, Monitoring and Evaluation');
+(2, 'Office of The President', 'Campus Planning, Monitoring and Evaluation'),
+(4, 'Top Management', 'Office of the President'),
+(5, 'Top Management', 'VP for Academic Affairs'),
+(6, 'Top Management', 'VP for Admin and Finance'),
+(7, 'Top Management', 'VP for RDEP'),
+(8, 'Top Management', 'Campus Directors'),
+(9, 'Office of The President', 'University Management Information System'),
+(10, 'Office of The President', 'International Development and Special Programs'),
+(11, 'Office of The President', 'Center for Life Long Learning'),
+(12, 'Office of The President', 'Campus Sports Development'),
+(13, 'Office of The President', 'Culture and Arts'),
+(14, 'Office of The President', 'ISO Command Center'),
+(15, 'Office of The President', 'Document Control Center'),
+(16, 'Academic Affairs', 'College of Accountancy'),
+(17, 'Academic Affairs', 'College of Business'),
+(18, 'Academic Affairs', 'College of Computer Studies'),
+(19, 'Academic Affairs', 'College of Social Work and Community Development'),
+(20, 'Academic Affairs', 'Graduate School'),
+(21, 'Academic Affairs', 'General Education Center'),
+(22, 'Academic Affairs', 'Laboratory Schools'),
+(23, 'Administration and Finance Division', 'Internal Audit Services'),
+(24, 'Research, Development, Extension, and Production Development', 'Campus Research');
 
 -- --------------------------------------------------------
 
@@ -360,11 +368,23 @@ ALTER TABLE `tbl_questionaireform`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `tbl_report`
+--
+ALTER TABLE `tbl_report`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `tbl_responses`
 --
 ALTER TABLE `tbl_responses`
   ADD PRIMARY KEY (`id`),
   ADD KEY `question_id` (`question_id`);
+
+--
+-- Indexes for table `tbl_tally_report`
+--
+ALTER TABLE `tbl_tally_report`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `tbl_unit`
@@ -393,13 +413,13 @@ ALTER TABLE `two_factor_codes`
 -- AUTO_INCREMENT for table `credentials`
 --
 ALTER TABLE `credentials`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `tbl_backup`
 --
 ALTER TABLE `tbl_backup`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `tbl_campus`
@@ -411,7 +431,7 @@ ALTER TABLE `tbl_campus`
 -- AUTO_INCREMENT for table `tbl_choices`
 --
 ALTER TABLE `tbl_choices`
-  MODIFY `choices_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=215;
+  MODIFY `choices_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `tbl_customer_type`
@@ -423,19 +443,25 @@ ALTER TABLE `tbl_customer_type`
 -- AUTO_INCREMENT for table `tbl_division`
 --
 ALTER TABLE `tbl_division`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `tbl_questionaire`
 --
 ALTER TABLE `tbl_questionaire`
-  MODIFY `question_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=71;
+  MODIFY `question_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `tbl_questionaireform`
 --
 ALTER TABLE `tbl_questionaireform`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_report`
+--
+ALTER TABLE `tbl_report`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `tbl_responses`
@@ -444,22 +470,28 @@ ALTER TABLE `tbl_responses`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `tbl_tally_report`
+--
+ALTER TABLE `tbl_tally_report`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `tbl_unit`
 --
 ALTER TABLE `tbl_unit`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `tbl_unit_mis`
 --
 ALTER TABLE `tbl_unit_mis`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT for table `two_factor_codes`
 --
 ALTER TABLE `two_factor_codes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=77;
 
 --
 -- Constraints for dumped tables
