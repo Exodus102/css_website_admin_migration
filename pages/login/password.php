69e952506816286065652c47c3ac0627cf1f2e99
Exodus102 <145414466+Exodus102@users.noreply.github.com>
2025-11-27 23:50:04 +0800

    27/11/2025 DD/MM/YYYY - Jenrick Aran

--- pages/login/password.php
+++ pages/login/password.php
@@ -32,7 +32,7 @@ if ($remaining <= 0 && $lockout_time > 0) {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../../Tailwind/src/output.css">
+  <link rel="stylesheet" href="../../Tailwind/public/output.css">
   <title>Customer Satisfaction</title>
 </head>
 
--- pages/login/two-factor-authentication.php
+++ pages/login/two-factor-authentication.php
@@ -13,7 +13,7 @@ if (!isset($_SESSION['user_authenticated_pending']) || !$_SESSION['user_authenti
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../../Tailwind/src/output.css">
+  <link rel="stylesheet" href="../../Tailwind/public/output.css">
   <script>
     // Apply saved font size on every page load
     (function() {
